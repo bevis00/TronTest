b1cd1b012ab3feb1d95fbc1a4124f40aa6ad90d3
bevis00 <bevis5555555@gmail.com>
2017-12-13 00:19:12 +0800

    2017/12/13

--- build.js
+++ build.js
@@ -250,7 +250,7 @@ function buildBigBall() {
   pickables.push(bigBall);
 
   var wireframeBall = new THREE.Mesh(new THREE.SphereGeometry(40.7,24,12), new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true, transparent: true } ));
-  wireframeBall.visible = false;
+
   scene.add(bigBall, wireframeBall);
 
 }
@@ -300,22 +300,6 @@ function buildLight() {
   var pointLight4 = new THREE.PointLight( 0xffffff, 1, 115 );
 	pointLight4.position.set( 37.5, -37.5, 37.5 );
 	scene.add( pointLight4 );
-
-  var pointLight5 = new THREE.PointLight( 0xffffff, 1, 115 );
-  pointLight5.position.set( -37.5, -37.5, -37.5 );
-  scene.add( pointLight5 );
-
-  var pointLight6 = new THREE.PointLight( 0xffffff, 1, 115 );
-  pointLight6.position.set( -37.5, -37.5, 37.5 );
-  scene.add( pointLight6 );
-
-  var pointLight7 = new THREE.PointLight( 0xffffff, 1, 115 );
-  pointLight7.position.set( 37.5, 37.5, -37.5 );
-  scene.add( pointLight7 );
-
-  var pointLight8 = new THREE.PointLight( 0xffffff, 1, 115 );
-  pointLight8.position.set( 37.5, 37.5, 37.5 );
-  scene.add( pointLight8 );
 
   var pointLightMid = new THREE.PointLight (0xffffff, 1, 200);
   pointLightMid.position.set(0,0,0);
